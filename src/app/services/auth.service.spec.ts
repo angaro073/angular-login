689c886cf7d8c7801b7c054a4a708f2b5d8e5d93
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-04-17 21:17:40 +0100

    AuthService testing setup

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { HttpClientTestingModule, TestRequest, } from '@angular/common/http/testing';
+import { HttpTestingController } from '@angular/common/http/testing';
+import { User } from '../user';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+	let httpTestController: HttpTestingController;
+	let mockRequest: TestRequest;
+	let mockUser: User;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule]
+		});
+    authService = TestBed.inject(AuthService);
+		httpTestController = TestBed.inject(HttpTestingController);
   });
 
+	afterEach(() => {
+		httpTestController.verify();
+	});
+
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(authService).toBeTruthy();
   });
+
+	it('should register a new user');
+	it('should log in an existing user');
+	it('should reset a user password');
+	it('should log out currently logged user')
+	it('should sign out currently logged user')
 });
